80948cfc064a3fb0fbd84cc9139ab3d68dca13c6
shafiqurrehman <shafiq.12@live.com>
2018-02-28 15:00:56 +0500

    Box component renderd

--- component-api/App.js
+++ component-api/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Counter from './component/Counter';
+import Box from './component/Box';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Counter color={'lightblue'} size={16} />
-        <Counter color={'skyblue'} size={26} />
         <Counter color={'darkblue'} size={36} />
         <Counter color={'steelblue'} size={46} />
+        <Box />
       </View>
     );
   }
